501a4b00bd8dca33c9da563187886b1138276ee6
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2024-03-23 20:31:31 +0530

    Added TokenInfo.jsx.

---
+++ src/components/authentication/TokenInfo.jsx
@@ -0,0 +1,30 @@
+
+import {useAuth0} from "@auth0/auth0-react";
+import {useState} from "react";
+
+const TokenInfo = () => {
+  const {user, getAccessTokenSilently} = useAuth0();
+  const {tokenString, setTokenString} = useState("empty");
+
+  const getAccessToken = async () => {
+      const token = await getAccessTokenSilently();
+      setTokenString(token);
+  }
+
+  return(
+      <div>
+          <button onClick={getAccessToken}>
+              Get Access Token
+          </button>
+
+          <br/>
+          <br/>
+
+          <text>
+              {tokenString}
+          </text>
+      </div>
+  );
+}
+
+export default TokenInfo;
