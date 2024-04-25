124af50d79a5ba92d6d3143d6e00db1c2a6734da
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2024-04-25 19:22:11 +0530

    Styling improvements.

--- src/pages/articles/ViewAllArticles.jsx
+++ src/pages/articles/ViewAllArticles.jsx
@@ -1,6 +1,7 @@
 import {Component} from "react";
 import ArticleService from "../../services/ArticleService";
-import {CardText} from "react-bootstrap";
+import {Card, CardText, CardTitle, Col, Row} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 
 class ViewAllArticles extends Component{
@@ -25,10 +26,38 @@ class ViewAllArticles extends Component{
 
     }
 
+    articleItemHeaderStyle = {
+        float : 'left',
+        textAlign: 'left',
+    }
+
+    articleItemSummaryStyle = {
+        float : 'left',
+        textAlign: 'left',
+    }
+
+    articleItemEmailStyle = {
+        float : 'left',
+        textAlign: 'left',
+        color : '#8C8AFF'
+    }
+    commentLinkStyle = {
+        float: 'left',
+        textAlign: 'left',
+        color : '#8C8AFF',
+        marginLeft: '20px'
+    }
+
+    readLinkStyle = {
+        float: 'left',
+        textAlign: 'left',
+        color : '#8C8AFF',
+    }
+
     render() {
         return (
             <div>
-                <div className={'container-fluid'}>
+                <div className={'container'}>
                     <h1>All Articles</h1>
                     <br/>
                     {
@@ -36,13 +65,36 @@ class ViewAllArticles extends Component{
                             <h3>No articles yet</h3> :
                             this.state.articles.map( (article) => (
                                 <div>
-                                    <h4>{article.title}</h4>
-                                    {/*<div dangerouslySetInnerHTML={{ __html:article.content }} />*/}
-                                    <div>
-                                        {article.summary}
-                                    </div>
+
+                                    <Card className={'card border-0 bg-transparent'}>
+                                        <Card.Header className={'bg-transparent border-0'}>
+                                            <h2 style={this.articleItemHeaderStyle}>{article.title}</h2>
+                                        </Card.Header>
+
+                                        <Card.Body className={'bg-transparent border-0'}>
+                                            <CardText style={this.articleItemEmailStyle}>{article.authorId}</CardText>
+                                            <CardText style={this.articleItemSummaryStyle}>{article.summary}</CardText>
+                                        </Card.Body>
+
+                                        <Card.Footer className={'border-0 bg-transparent'}>
+                                            <Link style={this.readLinkStyle} to={'/'}>Read</Link>
+                                            <Link style={this.commentLinkStyle} to={'/'}>Comment</Link>
+                                        </Card.Footer>
+
+                                    </Card>
 
                                     <br/>
+                                    <br/>
+                                    <br/>
+                                    <br/>
+
+                                    {/*<h4 style={this.articleItemHeaderStyle}>{article.title}</h4>
+                                    <p style={this.articleItemEmailStyle}>{article.authorId}</p>
+
+                                    <p style={this.articleItemSummaryStyle}>{article.summary}</p>*/}
+
+
+                                    {/*<div dangerouslySetInnerHTML={{ __html:article.content }} />*/}
                                 </div>
                             ))
                     }
